a1e7ba5558972ba1dfc3f1e73fbf1d0326d2f22c
yudi prayoga <yudi@technik.id>
2020-08-11 14:33:53 +0700

    fix typo api key

--- application/controllers/Produk.php
+++ application/controllers/Produk.php
@@ -3,6 +3,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 require APPPATH . '/libraries/REST_Controller.php';
+require 'C:\xampp\htdocs\midtrans_ci\vendor\midtrans\midtrans-php\Midtrans.php';
 
 use Restserver\Libraries\REST_Controller;
 
@@ -11,38 +12,56 @@ class Produk extends REST_Controller {
     function __construct($config = 'rest') {
         parent::__construct($config);
         $this->load->database();
+        $params = array('server_key' => 'SB-Mid-server-IZ4SpVcYVxSaLlzp3xhsiVHw', 'production' => false);
+		$this->load->library('midtrans');
+		$this->midtrans->config($params);
+		$this->load->helper('url');	
     }
 
 
     //Menampilkan welcome message
-    function index_get() {
+    function index_post() {
         $message = "welcome to checkout API";
-        $this->response($message, 200);
+        $checkout = array (
+            'customer_name' => $this->post('customer_name'),
+            'customer_email' => $this->post('customer_email'),
+            'customer_phone' => $this->post('customer_phone'),
+            'total_price' => floatval($this->post('total_price')),
+            'product_description' => $this->post('product_description'),
+        );
+
+        $transaction_details = array(
+            'order_id'      => rand(),
+            'total price'  => floatval($this->post('total_price')),
+        );
+        // Optional
+        $item_details = array (
+                'id'       => rand(),
+                        'price'    => floatval($this->post('total_price')),
+                        'quantity' => 1,
+                        'name'     => $this->post('product_description'),
+          );
+        // Optional
+        $customer_details = array(
+            'first_name'    => $this->post('customer_name'),
+                    'email'         =>  $this->post('customer_email'),
+                    'phone'         =>  $this->post('customer_phone'),
+        );
+        // Fill transaction details
+        $transaction = array(
+            'transaction_details' => $transaction_details,
+            'customer_details' => $customer_details,
+            'item_details' => $item_details,
+        );
+        // Data yang akan dikirim untuk request redirect_url.
+        $credit_card['secure'] = true;
+        //ser save_card true to enable oneclick or 2click
+        //$credit_card['save_card'] = true;
+
+        $snapToken =  $this->midtrans->getSnapToken($transaction);	
+        $this->response($snapToken, 200);
     }
 
-    function index_post() {
-     
-        $data = array(
-                    'id'           => $this->post('id'),
-                    'nama_produk'          => $this->post('nama_produk'),
-                    'tipe_produk'    => $this->post('tipe_produk'),
-                    'harga'    => $this->post('harga'),
-                    'stok'    => $this->post('stok'));
-                    // Required
-		$transaction_details = array(
-            'order_id' => rand(),
-            'gross_amount' => 95000, // no decimal allowed for creditcard
-          );
-        //   $snapToken = $this->midtrans->getSnapToken($data);
-		// error_log($snapToken);
-		// echo $snapToken;
-        $insert = $this->db->insert('produk', $data);
-        if ($insert) {
-            $this->response($data, 200);
-        } else {
-            $this->response(array('status' => 'fail', 502));
-        }
-    }
 }
 
 ?>
--- application/controllers/snap.php
+++ application/controllers/snap.php
@@ -2,7 +2,7 @@
 
 
 require APPPATH . '/libraries/REST_Controller.php';
-require APPPATH . '/libraries/Midtrans.php';
+require 'C:\xampp\htdocs\midtrans_ci\vendor\midtrans\midtrans-php\Midtrans.php';
 use Restserver\Libraries\REST_Controller;
 use Midtrans\Libraries\Midtrans;
 
@@ -29,7 +29,7 @@ class Snap extends REST_Controller {
     {
 		parent::__construct($config);
 		$this->load->database();
-        $params = array('server_key' => 'MIDTRANS_SERVERKEY=SB-Mid-server-IZ4SpVcYVxSaLlzp3xhsiVHw', 'production' => false);
+        $params = array('server_key' => 'SB-Mid-server-IZ4SpVcYVxSaLlzp3xhsiVHw', 'production' => false);
 		$this->load->library('midtrans');
 		$this->midtrans->config($params);
 		$this->load->helper('url');	
@@ -39,120 +39,47 @@ class Snap extends REST_Controller {
     {
     	$message = "welcome to checkout API";
 			// Required
+			// $transaction_details = array(
+			//   'order_id' => rand(),
+			//   'gross_amount' => 94000, // no decimal allowed for creditcard
+			// );
+			// // Data yang akan dikirim untuk request redirect_url.
+			// $credit_card['secure'] = true;
+			// //ser save_card true to enable oneclick or 2click
+			// //$credit_card['save_card'] = true;
+			// $transaction_data = array(
+			// 	'transaction_details'=> $transaction_details,
+			// );
+			// $snapToken =  $this->midtrans->getSnapToken($transaction_data);	
+			$this->response($message, 200);
+	}
+	
+	public function index_post()
+    {
+    	$message = "welcome to checkout API";
+			// Required
 			$transaction_details = array(
 			  'order_id' => rand(),
-			  'gross_amount' => 94000, // no decimal allowed for creditcard
+			  'customer_name' => $this->post('customer_name'),
+			  'customer_email' => $this->post('customer_email'),
+			  'customer_phone' => $this->post('customer_phone'),
+			  'product_description' => $this->post('product_description'),
+			  'gross_amount' => $this->post('total_price'), // no decimal allowed for creditcard
+
 			);
-	
-			// Optional
-			$item1_details = array(
-			  'id' => 'a1',
-			  'price' => 18000,
-			  'quantity' => 3,
-			  'name' => "Apple"
-			);
-	
-			// Optional
-			$item2_details = array(
-			  'id' => 'a2',
-			  'price' => 20000,
-			  'quantity' => 2,
-			  'name' => "Orange"
-			);
-	
-			// Optional
-			$item_details = array ($item1_details, $item2_details);
-	
-			// Optional
-			$billing_address = array(
-			  'first_name'    => "Andri",
-			  'last_name'     => "Litani",
-			  'address'       => "Mangga 20",
-			  'city'          => "Jakarta",
-			  'postal_code'   => "16602",
-			  'phone'         => "081122334455",
-			  'country_code'  => 'IDN'
-			);
-	
-			// Optional
-			$shipping_address = array(
-			  'first_name'    => "Obet",
-			  'last_name'     => "Supriadi",
-			  'address'       => "Manggis 90",
-			  'city'          => "Jakarta",
-			  'postal_code'   => "16601",
-			  'phone'         => "08113366345",
-			  'country_code'  => 'IDN'
-			);
-	
-			// Optional
-			$customer_details = array(
-			  'first_name'    => "Andri",
-			  'last_name'     => "Litani",
-			  'email'         => "andri@litani.com",
-			  'phone'         => "081122334455",
-			  'billing_address'  => $billing_address,
-			  'shipping_address' => $shipping_address
-			);
-	
 			// Data yang akan dikirim untuk request redirect_url.
 			$credit_card['secure'] = true;
 			//ser save_card true to enable oneclick or 2click
 			//$credit_card['save_card'] = true;
-	
-			$time = time();
-			$custom_expiry = array(
-				'start_time' => date("Y-m-d H:i:s O",$time),
-				'unit' => 'minute', 
-				'duration'  => 2
-			);
-			
 			$transaction_data = array(
 				'transaction_details'=> $transaction_details,
-				'item_details'       => $item_details,
-				'customer_details'   => $customer_details,
-				'credit_card'        => $credit_card,
-				'expiry'             => $custom_expiry
 			);
+			$snapToken =  $this->midtrans->getSnapToken($transaction_data);	
+			$transaction_data = $this->response($transaction_data);
+			$link_token=$this->response($snapToken);
+			echo $link_token;
+			echo $transaction_data;
+	}
 	
-			// $snapToken = $this->midtrans->getSnapToken($transaction_data);
-		
-			$this->response($transaction_data, 200);
-    }
-
-	public function index_post()
-    {
-		$message = "test run";
-		// Required
-		$transaction_details = array(
-		  'order_id' => rand(),
-		  'gross_amount' => 94000, // no decimal allowed for creditcard
-		);
-
-
-		// Data yang akan dikirim untuk request redirect_url.
-        $credit_card['secure'] = true;
-        //ser save_card true to enable oneclick or 2click
-        //$credit_card['save_card'] = true;
-
-        $time = time();
-        $custom_expiry = array(
-            'start_time' => date("Y-m-d H:i:s O",$time),
-            'unit' => 'minute', 
-            'duration'  => 2
-        );
-        
-        $transaction_data = array(
-            'transaction_details'=> $transaction_details,
-            'credit_card'        => $credit_card,
-            'expiry'             => $custom_expiry
-        );
-
-		error_log(json_encode($transaction_data));
-		$snapToken = $this->midtrans->getSnapToken($transaction_data);
-		error_log($snapToken);
-		echo $snapToken;
-		$this->response($snapToken, 200);
-    }
 }
 ?>
